92a09319d841c6a8aa61fa6198623b1163ac68e4
jdwheels <Wheeler.John.D@gmail.com>
2019-03-26 15:29:46 -0400

    Migrate Chain to Class

--- lib/chain.ts
+++ lib/chain.ts
@@ -22,27 +22,28 @@
  * SOFTWARE.
  */
 
-function Chain() {
-  var self = this;
-  this.queue = [];
-  this.error = function(err) {
+export default class Chain {
+  private queue = [];
+
+  private error = (err) => {
 
   };
-  this.next = function(data) {
-    var func = self.queue.shift();
+
+  private next = (data?) => {
+    const func = this.queue.shift();
     if (func) {
-      func(self.error, self.next, data);
+      func(this.error, this.next, data);
     }
   };
+
+  public then = (func) => {
+    this.queue.push(func);
+    return this;
+  };
+
+  public execute = () => {
+    this.next();
+  };
 }
-
-Chain.prototype.then = function(func) {
-  this.queue.push(func);
-  return this;
-};
-
-Chain.prototype.execute = function() {
-  this.next();
-};
 
 module.exports = Chain;
